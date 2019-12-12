0c4ee8021c822200cbafb376eb397e8b73f51aa7
Evan Emelga <evan.emelga@mokapos.com>
2019-12-12 11:39:40 +0700

    Bump to 2.0.8 deps

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]), // dev
     ],
     dependencies: [
-        .package(url: "https://github.com/mokapos/danger-swift.git", from: "2.0.7"),
+        .package(url: "https://github.com/mokapos/danger-swift.git", from: "2.0.8"),
         // Dev dependencies
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.35.8"), // dev
         .package(url: "https://github.com/Realm/SwiftLint", from: "0.28.1"), // dev
